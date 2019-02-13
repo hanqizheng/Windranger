2f43e557379474129c431a529823f47dd4d06225
hanqizheng <hanqizheng598@gmail.com>
2019-02-13 19:11:57 +0800

    a test windranger-server to response the request and request the aim url

---
+++ Windranger-server/app/controller/handler.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class HomeController extends Controller {
+  async handleLocalRequest() {
+    const url = ctx.request.body.url;
+    const method = ctx.request.body.method;
+    const data = await ctx.service.handler.proxy(url, method);
+    ctx.body = data;
+  }
+}
+
+module.exports = HomeController;
---
+++ Windranger-server/app/service/handler.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const Service = require('egg').Service;
+
+class HandlerService extends Service {
+  async proxy() {
+    
+  }
+}
+
+module.exports = HandlerService;
