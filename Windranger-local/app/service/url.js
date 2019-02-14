72a78cd64a93e05c906d2b7756a8790cc37a7f06
hanqizheng <hanqizheng598@gmail.com>
2019-02-14 09:42:57 +0800

    delete the wrong file

--- Windranger-local/app/service/url.js
+++ Windranger-local/app/service/url.js
@@ -2,27 +2,43 @@
 
 const Service = require('egg').Service;
 const https = require('https');
+const querystring = require('querystring');
 
 class UrlService extends Service {
   async requestRemoteServer(url, method) {
-    // console.log(method);
-    if (method === 'GET') {
-      https.get(url, res => {
-        let data = '';
+    const reqBody = querystring.stringify({
+      'url': url,
+      'method': method
+    });
 
-        res.on('data', chunk => {
-          data += chunk;
-        });
-
-        res.on('end', () => {
-          return data;
-        });
-      }).on('error', error => {
-        console.log(error);
-      });
-    } else if (method === 'POST') {
-      console.log('post request');
+    const option = {
+      hostname: '149.28.74.184',
+      port: '7001',
+      path: '/request',
+      method: 'POST',
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(reqBody)
+      }
     }
+
+    const req = https.request(option, res => {
+      res.setEncoding('utf8');
+      res.on('data', (chunk) => {
+        console.log(`BODY: ${chunk}`);
+      });
+      res.on('end', () => {
+        console.log('No more data in response.');
+      });
+    })
+
+    req.on('error', (e) => {
+      console.error(`problem with request: ${e.message}`);
+    });
+    
+    // write data to request body
+    req.write(reqBody);
+    req.end();
   }
 }
 
