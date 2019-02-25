fe2e1be16cd8639ff500c2d0aaa69a2d214bbe17
hanqizheng <hanqizheng598@gmail.com>
2019-02-25 19:54:22 +0800

    encrypt not finished

--- Windranger-local/app/controller/url.js
+++ Windranger-local/app/controller/url.js
@@ -4,9 +4,12 @@ const Controller = require('egg').Controller;
 
 class UrlController extends Controller {
   async getURL(ctx) {
-    const url = ctx.request.body.url;
-    const method = ctx.request.body.method;
-    const result = await ctx.service.url.requestRemoteServer(url, method);
+    // const url = ctx.request.body.url;
+    // const method = ctx.request.body.method;
+    // detai 就是所有请求的内容
+    const detail = ctx.request.body.detail;
+    // const encryptDetail = await ctx.service.cipher.
+    // const result = await ctx.service.url.requestRemoteServer(detail);
     console.log(7777, result);
     ctx.body = result;
   }
--- Windranger-local/app/service/securitySocket.js
+++ Windranger-local/app/service/securitySocket.js
@@ -3,9 +3,31 @@
 const Service = require('egg').Service;
 
 class SecurityService extends Service {
-  async encodeBuffer(buffer) {
-    
+  async encodeBuffer(chunk) {
+    const { ctx } = this;
+    if (!chunk) {
+      return;
+    }
+    const encode = await ctx.service.cipher.encode(chunk);
+    return encode;
   }
+
+  async decodeBuffer(chunk) {
+    const { ctx } = this;
+    if (!chunk) {
+      return;
+    }
+    const decode = await ctx.service.cipher.decode(chunk);
+    return decode;
+  }
+
+  // async socketWrite(socket, buffer) {
+
+  // }
+
+  // async socketRead(socket, buffer) {
+    
+  // }
 }
 
 module.exports = SecurityService;
